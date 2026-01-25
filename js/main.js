26a97509f8e700f3352282c71aa180b65ec2f0fb
Mohandahss S <mohandhass123s@gmail.com>
2026-01-25 12:08:04 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.querySelector('#start-btn');
     const ui = document.querySelector('#ui');
     const hud = document.querySelector('#hud-container');
-    const video = document.querySelector('#ar-video');
+    const iconLayer = document.querySelector('#client-icon-layer');
+    const arVideo = document.querySelector('#ar-video');
     const scannerVideo = document.querySelector('#scanner-video');
     const sceneEl = document.querySelector('a-scene');
     const target = document.querySelector('#target');
@@ -10,21 +11,19 @@ document.addEventListener('DOMContentLoaded', () => {
     startBtn.addEventListener('click', () => {
         ui.style.display = 'none';
         hud.style.display = 'block';
+        iconLayer.style.display = 'flex'; // Shows the round buttons
 
-        // Start AR
         sceneEl.systems['mindar-image-system'].start();
-        
-        // Play scanner overlay
         scannerVideo.play();
     });
 
     target.addEventListener("targetFound", () => {
         hud.style.display = 'none';
-        video.play();
+        arVideo.play();
     });
 
     target.addEventListener("targetLost", () => {
         hud.style.display = 'block';
-        video.pause();
+        arVideo.pause();
     });
 });
