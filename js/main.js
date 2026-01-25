4ab8d130531b5ba48f53ebac12875f81c123c196
Mohandahss S <mohandhass123s@gmail.com>
2026-01-25 14:17:45 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const startBtn = document.querySelector('#start-btn');
-    const ui = document.querySelector('#ui');
-    const hud = document.querySelector('#hud-container');
-    const iconLayer = document.querySelector('#client-icon-layer');
-    const arVideo = document.querySelector('#ar-video');
-    const scannerVideo = document.querySelector('#scanner-video');
+    const startBtn = document.getElementById('start-btn');
+    const ui = document.getElementById('ui');
+    const hud = document.getElementById('hud-container');
+    const icons = document.getElementById('client-icon-layer');
+    const scannerVideo = document.getElementById('scanner-video');
     const sceneEl = document.querySelector('a-scene');
-    const target = document.querySelector('#target');
 
     startBtn.addEventListener('click', () => {
         ui.style.display = 'none';
         hud.style.display = 'block';
-        iconLayer.style.display = 'flex'; // Shows the round buttons
-
+        icons.style.display = 'flex';
         sceneEl.systems['mindar-image-system'].start();
         scannerVideo.play();
-    });
-
-    target.addEventListener("targetFound", () => {
-        hud.style.display = 'none';
-        arVideo.play();
-    });
-
-    target.addEventListener("targetLost", () => {
-        hud.style.display = 'block';
-        arVideo.pause();
     });
 });
