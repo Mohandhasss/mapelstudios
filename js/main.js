a7d0152f2ea10a21cef359febf33949d1a53646a
Mohandahss S <mohandhass123s@gmail.com>
2026-01-25 15:07:25 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.getElementById('start-btn');
     const ui = document.getElementById('ui');
     const hud = document.getElementById('hud-container');
-    const icons = document.getElementById('client-icon-layer');
+    const iconLayer = document.getElementById('client-icon-layer');
     const scannerVideo = document.getElementById('scanner-video');
     const sceneEl = document.querySelector('a-scene');
 
     startBtn.addEventListener('click', () => {
         ui.style.display = 'none';
         hud.style.display = 'block';
-        icons.style.display = 'flex';
+        iconLayer.style.display = 'flex';
         sceneEl.systems['mindar-image-system'].start();
         scannerVideo.play();
     });
